947604b6abd83372089f5e21e52463df064fcb28
gl1tchs <berkeumtbiricik@gmail.com>
2024-02-24 16:04:44 +0300

    add: create file and folder menu to content browser.

--- editor/panels/content_browser.cpp
+++ editor/panels/content_browser.cpp
@@ -12,21 +12,20 @@ ContentBrowserPanel::ContentBrowserPanel() {
 	set_flags(ImGuiWindowFlags_HorizontalScrollbar);
 }
 
-ContentBrowserPanel::~ContentBrowserPanel() {
-}
+ContentBrowserPanel::~ContentBrowserPanel() {}
 
 // get unique new scene path
-inline static std::string get_new_scene_path() {
+inline static std::string get_new_file_path(
+		const char* file_name, const char* extension = "") {
 	static uint32_t iteration = 0;
 
-	std::string path = iteration > 0
-			? std::format("res://new_scene{}.escn", iteration)
-			: "res://new_scene.escn";
+	std::string path = std::format("res://{}{}.{}", file_name,
+			iteration > 0 ? std::to_string(iteration) : "", extension);
 
 	const fs::path fs_path = Project::get_asset_path(path);
 	if (fs::exists(fs_path)) {
 		iteration++;
-		return get_new_scene_path();
+		return get_new_file_path(file_name, extension);
 	}
 
 	iteration++;
@@ -44,31 +43,40 @@ void ContentBrowserPanel::_draw() {
 		return;
 	}
 
-	if (ImGui::TreeNodeEx("res://", ImGuiTreeNodeFlags_OpenOnArrow | ImGuiTreeNodeFlags_DefaultOpen)) {
-		idx = 0;
-		for (const auto& subpath : fs::directory_iterator(asset_directory)) {
-			_draw_file(subpath);
-		}
-
-		// Clicked somewhere else, reset selected_idx to -1
-		if (is_focused() && !ImGui::IsItemHovered() &&
-				ImGui::IsMouseClicked(ImGuiMouseButton_Left)) {
-			selected_idx = -1;
-		}
-
-		ImGui::TreePop();
+	idx = 0;
+	for (const auto& subpath : fs::directory_iterator(asset_directory)) {
+		_draw_file(subpath);
 	}
 
-	ImGui::Dummy(
-			ImVec2(ImGui::GetWindowWidth(), std::max(ImGui::GetContentRegionAvail().y, 100.0f)));
+	// Clicked somewhere else, reset selected_idx to -1
+	if (is_focused() && !ImGui::IsItemHovered() &&
+			ImGui::IsMouseClicked(ImGuiMouseButton_Left)) {
+		selected_idx = -1;
+	}
+
+	ImGui::Dummy(ImVec2(ImGui::GetWindowWidth(),
+			std::max(ImGui::GetContentRegionAvail().y, 100.0f)));
 
 	if (ImGui::BeginPopupContextItem("ContentBrowserContextMenu")) {
 		if (ImGui::BeginMenu("Create")) {
 			if (ImGui::MenuItem("Scene")) {
-				Scene::serialize(create_ref<Scene>(), get_new_scene_path());
+				Scene::serialize(create_ref<Scene>(),
+						get_new_file_path("new_scene", "escn"));
 			}
 
 			ImGui::EndMenu();
+		}
+
+		if (ImGui::MenuItem("New File")) {
+			const fs::path file_path = Project::get_asset_path(
+					get_new_file_path("new_file", "txt"));
+			std::ofstream file(file_path);
+			file.close();
+		}
+
+		if (ImGui::MenuItem("New Folder")) {
+			const std::string file_path = get_new_file_path("new_folder", "");
+			fs::create_directories(Project::get_asset_path(file_path));
 		}
 
 		if (ImGui::MenuItem("Open In Explorer")) {
@@ -79,7 +87,8 @@ void ContentBrowserPanel::_draw() {
 	}
 
 	// reset renaming index
-	if (ImGui::IsItemClicked() || (renaming_idx != -1 && ImGui::IsKeyPressed(ImGuiKey_Escape))) {
+	if (ImGui::IsItemClicked() ||
+			(renaming_idx != -1 && ImGui::IsKeyPressed(ImGuiKey_Escape))) {
 		renaming_idx = -1;
 	}
 }
@@ -105,11 +114,14 @@ void ContentBrowserPanel::_draw_file(const fs::path& path) {
 
 	// is file an asset or not
 	if (!fs::is_directory(path)) {
-		const AssetHandle handle = scene->get_asset_registry().get_handle_from_path(path.string());
-		const bool is_loaded = handle && scene->get_asset_registry().is_asset_loaded(handle);
+		const AssetHandle handle =
+				scene->get_asset_registry().get_handle_from_path(path.string());
+		const bool is_loaded =
+				handle && scene->get_asset_registry().is_asset_loaded(handle);
 
 		if (!is_renaming) {
-			const std::string label = std::format("{1}  {0}", filename, (is_loaded ? ICON_FA_CIRCLE : ICON_FA_CIRCLE_O));
+			const std::string label = std::format("{1}  {0}", filename,
+					(is_loaded ? ICON_FA_CIRCLE : ICON_FA_CIRCLE_O));
 			if (ImGui::Selectable(label.c_str(), is_selected)) {
 				selected_idx = is_selected ? -1 : idx;
 			}
@@ -119,7 +131,8 @@ void ContentBrowserPanel::_draw_file(const fs::path& path) {
 			_draw_rename_file_dialog(path);
 		}
 
-		const AssetType type = get_asset_type_from_extension(path.extension().string());
+		const AssetType type =
+				get_asset_type_from_extension(path.extension().string());
 
 		// make scenes importable just by dragging into viewport
 		if (!is_loaded && type == AssetType::SCENE) {
@@ -138,8 +151,8 @@ void ContentBrowserPanel::_draw_file(const fs::path& path) {
 				const std::string payload_name =
 						"DND_PAYLOAD_" + serialize_asset_type(type);
 
-				ImGui::SetDragDropPayload(payload_name.c_str(), &handle,
-						sizeof(AssetHandle));
+				ImGui::SetDragDropPayload(
+						payload_name.c_str(), &handle, sizeof(AssetHandle));
 
 				ImGui::SetTooltip("%s", serialize_asset_type(type).c_str());
 
@@ -149,11 +162,12 @@ void ContentBrowserPanel::_draw_file(const fs::path& path) {
 	} else {
 		if (!is_renaming) {
 			bool open = ImGui::TreeNodeEx(filename.c_str(),
-					ImGuiTreeNodeFlags_OpenOnArrow | (is_selected ? ImGuiTreeNodeFlags_Selected : 0));
+					ImGuiTreeNodeFlags_OpenOnArrow |
+							(is_selected ? ImGuiTreeNodeFlags_Selected : 0));
 
 			_draw_popup_context(path);
 
-			if (open) {
+			if (open && fs::exists(path)) {
 				// iterate throught elements
 				for (const auto& subpath : fs::directory_iterator(path)) {
 					idx++;
@@ -183,7 +197,8 @@ void ContentBrowserPanel::_draw_rename_file_dialog(const fs::path& path) {
 	}
 
 	std::string new_name = path.filename().string();
-	if (ImGui::InputText("##RenameInput", &new_name, ImGuiInputTextFlags_EnterReturnsTrue)) {
+	if (ImGui::InputText("##RenameInput", &new_name,
+				ImGuiInputTextFlags_EnterReturnsTrue)) {
 		fs::path new_path = path.parent_path() / new_name;
 
 		// replace the extension so that they would be same
@@ -206,17 +221,21 @@ void ContentBrowserPanel::_draw_popup_context(const fs::path& path) {
 
 	if (ImGui::BeginPopupContextItem()) {
 		// import if not asset
-		const AssetType type = get_asset_type_from_extension(path.extension().string());
+		const AssetType type =
+				get_asset_type_from_extension(path.extension().string());
 
 		// do not let scenes to be imported here
-		if ((type != AssetType::NONE && type != AssetType::SCENE) && ImGui::MenuItem("Load")) {
+		if ((type != AssetType::NONE && type != AssetType::SCENE) &&
+				ImGui::MenuItem("Load")) {
 			scene->get_asset_registry().load_asset(
-					Project::get_relative_asset_path(path.string()),
-					type);
+					Project::get_relative_asset_path(path.string()), type);
 		}
 
-		if (const AssetHandle handle = scene->get_asset_registry().get_handle_from_path(path.string());
-				scene->get_asset_registry().is_asset_loaded(handle) && ImGui::MenuItem("Unload")) {
+		if (const AssetHandle handle =
+						scene->get_asset_registry().get_handle_from_path(
+								path.string());
+				scene->get_asset_registry().is_asset_loaded(handle) &&
+				ImGui::MenuItem("Unload")) {
 			scene->get_asset_registry().unload_asset(handle);
 		}
 
@@ -233,5 +252,6 @@ void ContentBrowserPanel::_draw_popup_context(const fs::path& path) {
 }
 
 bool ContentBrowserPanel::_is_asset_file(const fs::path& path) {
-	return get_asset_type_from_extension(path.extension().string()) != AssetType::NONE;
+	return get_asset_type_from_extension(path.extension().string()) !=
+			AssetType::NONE;
 }
